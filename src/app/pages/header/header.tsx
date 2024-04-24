5d0dc2cd73d0bb6c6f6537e592017780f2dbb728
uriel <pozouriel@gmail.com>
2024-04-24 22:33:20 +0200

    cambio nombre del logo

--- src/app/pages/header/header.tsx
+++ src/app/pages/header/header.tsx
@@ -7,7 +7,7 @@ const Header: React.FC = () => {
   return (
     <header className={styles.header}>
       <div className={styles.logoContainer}>
-        <img src="/images/Logo_kaveHome.png" alt="Logo" className={styles.logo} />
+        <img src="/images/logo.png" alt="Logo" className={styles.logo} />
       </div>
       <div className={styles.iconContainer}>
         {/* <img src="/images/heart.png" alt="Heart" className={styles.heartIcon} /> */}
