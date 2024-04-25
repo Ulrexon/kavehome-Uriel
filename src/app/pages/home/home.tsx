ef153800e17452f862a87e591d51a36e0978a5e3
uriel <pozouriel@gmail.com>
2024-04-25 21:24:09 +0200

    he preparado un ejemplo con imagenes y centrado el contenido de unas cards que he creado las cuales para poder visualizar todas se ha de desplazar en el caso que fuera mas pequeña la web

--- src/app/pages/home/home.tsx
+++ src/app/pages/home/home.tsx
@@ -6,8 +6,11 @@ import styles from './homeStyles.module.scss';
 const Home: React.FC = () => {
   return (
     <div className="home-container">
+
       <img className={styles.homeImage} src="/images/imagenhome.png" alt="Home Image" />
+      
       <h2 style={{ textAlign: 'center'}}>Inspírate</h2>
+
       <div className={styles.botoneraContainer}>
         <div className={styles.button1}>Estancias</div>
         <div className={styles.button1}>Estil</div>
@@ -16,6 +19,26 @@ const Home: React.FC = () => {
         <div className={styles.button1}>We are Kave</div>
         <div className={styles.button1}>Proyectos</div>
       </div>
+
+      <div className={styles.cardListContainer}>
+        <div className={styles.card}>
+          <img src="/images/ejemplo.jpg" alt="test" />
+          <p>Nombre del artículo</p>
+        </div>
+        <div className={styles.card}>
+          <img src="/images/ejemplo.jpg" alt="test" />
+          <p>Nombre del artículo</p>
+        </div>
+        <div className={styles.card}>
+          <img src="/images/ejemplo.jpg" alt="test" />
+          <p>Nombre del artículo</p>
+        </div>
+        <div className={styles.card}>
+          <img src="/images/ejemplo.jpg" alt="test" />
+          <p>Nombre del artículo</p>
+        </div>
+      </div>
+
     </div>    
   );
 }
